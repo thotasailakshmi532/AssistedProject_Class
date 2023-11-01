95ce7441cb040bf7a3712b0727d6ea3c3d254165
thotasailakshmi532 <thotasailakshmi532@gmail.com>
2023-11-01 10:21:29 +0530

    Add project files.

---
+++ ClassLibrary1/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Subject
+    {
+        private string name;
+        private string shortName;
+
+        public string Name
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+
+        public string ShortName
+        {
+            get { return shortName; }
+            set { shortName = value; }
+        }
+    }
+
+}
+
